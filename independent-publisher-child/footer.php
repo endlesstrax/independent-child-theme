9d8d30d31920467547aae41e2bf9ab763d467057
Ricky White <ricky1white@gmail.com>
2017-01-27 21:55:21 -0500

    Add files via upload

---
+++ independent-publisher-child/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Independent Publisher
+ * @since   Independent Publisher 1.0
+ */
+?>
+
+</div><!-- #main .site-main -->
+
+<!-- Custom Footer Code -->
+<footer class="site-footer">
+	<div class="my-footer-info">
+		<p>Copyright &copy <?php echo date("Y"); ?> — <a href="http://www.rickywhite.net" title="Ricky White">Ricky White</a>. All rights Reserved.</p>
+	</div>
+</footer> 
+<!-- END Custom footer -->
+
+</div><!-- #page .hfeed .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
